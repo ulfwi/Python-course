6138b9d793f899553c5fd599a97557ce02720957
linasjostrand <lina.sjostrand@hotmail.com>
2017-09-04 12:30:51 +0200

    Continued working on spline.py

--- Project1/spline.py
+++ Project1/spline.py
@@ -1,11 +1,32 @@
-import numpy
+import numpy as np
 
 import math
 
 class Spline:
 
-    def __init__(self, ):
-    def spline(self, *coord):
+    # x are node points (u)
+    # y are node function values
+    # d are deBoor points
+    # N
+    def __init__(self, x, y, step = 0.01):
+        self.x = x
+        self.y = y
+        self.step = step
+
+    def spline(self):
+        spline_vec = np.array([]);
+        #deBoor
+        return spline_vec
 
 
-    def basis(self):
+    # Creates basis functions N_i^k
+    def basis(self, i, k):
+
+    # Recursively evaluate the spline /deBoor algorithm
+    def deBoor(self):
+        #return d?
+
+    def plot(self):
+        xplot = np.arange(self.x[0], self.x[-1], self.step)
+
+        plot()
