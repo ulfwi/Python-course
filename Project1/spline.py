45fd71736145733382559a7af3bef1023ac61dc0
linasjostrand <lina.sjostrand@hotmail.com>
2017-09-05 15:55:23 +0200

    work on deBoor alg

--- Project1/spline.py
+++ Project1/spline.py
@@ -9,17 +9,19 @@ class Spline:
     # d are deBoor points
     # x
     # N
-    def __init__(self, u, y, step = 0.01):
+    def __init__(self, u, y, c, step = 0.01):
         self.u = u
         self.y = y
         self.step = step
+        self.c = self.u # vad i hela världen ska denna vara?
         length = u[-1] - u[0]
 
     def spline(self):
         spline_vec = np.array([])
         #deBoor
-        return spline_vec
 
+
+        return spline_vec
 
     # Creates basis functions N_i^k
     def basis(self, x, i):
@@ -28,8 +30,16 @@ class Spline:
 
 
     # Recursively evaluate the spline /deBoor algorithm
-    def deBoor(self):
+    def deBoorPoints(self, u_index):
+        u_short = self.u[2:-2]
+        d = np.array([np.zeros(4)])
+        alpha = np.array([np.zeros(4)])
+        for i in range(0, np.len(d)-1):
+            d[i] = self.c[u_index-3+i]
+            alpha[i] = u[u_index] -
         #return d?
+
+        # HUR VÄLJS CONTROL POINTS?? KOLLA MER PÅ DET.
 
     def plot(self):
         xplot = np.arange(self.x[0], self.x[-1], self.step)
