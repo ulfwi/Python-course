5c47a0497bad633a87dd40db645d9b209e4d4248
fannyandersson <a.fanny.andersson@gmail.com>
2017-09-08 10:27:24 +0200

    workning plot?

--- Project1/spline.py
+++ Project1/spline.py
@@ -11,7 +11,7 @@ class Spline:
     d are deBoor points
     N length of parameter vector
     """
-    def __init__(self, coord, u, interpolate=False, N=100):
+    def __init__(self, coord, u, interpolate=False, N=4): # 100
         self.u = u
         self.u.sort()  # if not sorted in increasing order
         # Create parameter vector t of length N
@@ -73,30 +73,47 @@ class Spline:
 
     def de_Boor_points(self, t_point):
         # Find hot interval
-        index = (t_point <= self.u).argmax() - 1  # u_I
-        if index < 0:
-            index = 0
+        # ind = (t_point <= self.u).argmax() - 1  # u_I
+        ind = (t_point <= self.u[2:-1]).argmax() + 2  # u_I
+        if ind < 0:
+            ind = 0
         # Select corresponding control points d_i
-        d_org = np.array([self.d[index - 2 + i] for i in range(4)])
-        alpha = self.div(self.u[index + 1] - t_point, self.u[index + 1] - self.u[index - 2]) # ALPHA SHOULD BE UPDATED, DEPENDS ON BLOSSOM PAIR
+        d_org = np.array([self.d[ind - 2 + i] for i in range(4)])
+        alpha = self.div(self.u[ind + 1] - t_point, self.u[ind + 1] - self.u[ind - 2]) # ALPHA SHOULD BE UPDATED, DEPENDS ON BLOSSOM PAIR
 
         # Blossom recursion, sec for second interpolation etc
-        d_sec = np.array([alpha * d_org[index - 2 + i] + (1 - alpha) * d_org[index - 1 + i] for i in range(d_org.shape[0] - 1)])
-        d_thr = np.array([alpha * d_sec[index - 2 + i] + (1 - alpha) * d_sec[index - 1 + i] for i in range(d_sec.shape[0] - 1)])
-        d_fou = np.array([alpha * d_thr[index - 2 + i] + (1 - alpha) * d_thr[index - 1 + i] for i in range(d_thr.shape[0] - 1)])
+        d_sec = np.array([alpha * d_org[i] + (1 - alpha) * d_org[1 + i] for i in range(d_org.shape[0] - 1)])
+        d_thr = np.array([alpha * d_sec[i] + (1 - alpha) * d_sec[1 + i] for i in range(d_sec.shape[0] - 1)])
+        d_fou = np.array([alpha * d_thr[i] + (1 - alpha) * d_thr[1 + i] for i in range(d_thr.shape[0] - 1)])
 
         return d_fou  # s(t)
 
-
+ #   d_sec = np.array([alpha * d_org[ind - 2 + i] + (1 - alpha) * d_org[ind - 1 + i] for i in range(d_org.shape[0] - 1)])
+ #   d_thr = np.array([alpha * d_sec[ind - 2 + i] + (1 - alpha) * d_sec[ind - 1 + i] for i in range(d_sec.shape[0] - 1)])
+ #   d_fou = np.array([alpha * d_thr[ind - 2 + i] + (1 - alpha) * d_thr[ind - 1 + i] for i in range(d_thr.shape[0] - 1)])
 
 
 
     # polygon: boolean, plot if true
-    def plotSpline(self, polygon):
+    def plotSpline(self, polygon=False):
         if polygon:
             plt.plot(self.d[0,:], self.d[1,:],'r-*')
+            print(self.d[0,:])
         s = self.spline()
-        plt.plot(s[0,:],s[1,:],'b')
+        print(s)
+        print(s[0,:])
+        for i in range(s.shape[0]):
+            print(s[i,0])
+            plt.plot(s[i,0],s[i,1],'b')
+        print("afterafter")
+        #plt.show()
+
+        fig = plt.gcf()
+        plotly_fig = tls.mpl_to_plotly(fig)
+        plotly_fig['layout']['showlegend'] = True
+        print("?")
+
+
 
     def div(self,x,y):
         if y == 0 and x == 0:
--- Project1/test_basis.py
+++ Project1/test_basis.py
@@ -1,12 +1,23 @@
 from spline import Spline
 import numpy as np
+import matplotlib.pyplot as plt
 
-d = np.array([[1,2],[1,2],[1,2],[1,2],[1,2],[1,2]])
-s = Spline(d)
 
+#d = np.array([[0,2],[1,3],[2,4],[3,3],[1,2],[0,0]])
+#u = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+d = np.array([[0,2],[1,3],[2,4],[3,3],[1,2],[0,0]])
+u = np.array([0, 1, 2])
+s = Spline(d,u)
+
+print("beore")
+s.plotSpline()
+print("after")
 x = 2
 i = 3
 N = s.basis(x,i)
 print(N)
+
+x = np.linspace(0, 10)
+plt.plot(x, np.sin(x), '--', linewidth=2)
 
 
