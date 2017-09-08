8bd5d1f1a40b5807f1cf5834a7a5706653846937
fannyandersson <a.fanny.andersson@gmail.com>
2017-09-08 11:08:24 +0200

    workning plot?

--- Project1/poltti.py
+++ Project1/poltti.py
@@ -1,5 +1,8 @@
+
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4], [1,4,9,16], 'ro')
-plt.axis([0, 6, 0, 20])
+plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
