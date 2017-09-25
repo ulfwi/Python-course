c688fa573f0ecebb90d3a2b319d73cca61b904c0
linasjostrand <lina.sjostrand@hotmail.com>
2017-09-25 10:37:10 +0200

    fixed

--- Project2/line_search_methods.py
+++ Project2/line_search_methods.py
@@ -1,5 +1,6 @@
 """
-Line search methods
+Line search methods.
+Exact line search and inexact (Wolfe, Goldstein)
 
 Authors: Fanny Andersson (tna13fan), Louise Sjöholm (tfy13lsj), Lina Sjöstrand (fra12lsj), Björn Ulfwi (tfy13bul)
 """
@@ -8,43 +9,48 @@ import scipy.optimize as op
 import numpy as np
 
 
-def ls_exact(self, p, x):
-    """
-    Exact line search. Minimize objective function func with respect to alpha (alp)
-    using scikit.optimize.fmin
-
-    :param p: direction of steepest descent
-    :param x: evaluation point (??????????????)
-    :return: step length alpha (alp)
-    """
-    alp = 0
-    alp = op.fmin(self.func_alpha, alp, args=(x, p), disp=False)
-    return alp
-
-
-def func_alpha(self, alp, x, p):
+def func_alpha(func, alp, x, p):
     """
     Define objective function as a function of alpha
+
+    :param func: objective function
     :param alp: step length
     :param p: search direction
     :param x: point
     :return: obj function evaluated in point (x + alpha*p)
     """
-    return self.func(x + alp * p)
+    return func(x + alp * p)
 
 
-def grad_alpha(self, alp, x, p):
+def grad_alpha(grad, alp, x, p):
     """
     Define derivative of objective function with respect to alpha
+
+    :param grad: gradient of objective function
     :param alp: step length
     :param p: search direction
     :param x: point
     :return: derivative of obj function evaluated in point (x + alpha*p)
     """
-    return np.dot(self.grad(x + alp * p), p)
+    return np.dot(grad(x + alp * p), p)
 
 
-def ls_block1(self, x, p, alp_0, alp_l, tau=0.1, chi=9):
+def ls_exact(func, x, p):
+    """
+    Exact line search. Minimize objective function func with respect to alpha (alp)
+    using scikit.optimize.fmin
+    
+    :param func: function to be minimized
+    :param p: direction of steepest descent
+    :param x: evaluation point (??????????????)
+    :return: step length alpha (alp)
+    """
+    alp = 0
+    alp = op.fmin(func_alpha, alp, args=(x, p), disp=False)
+    return alp
+
+
+def ls_block1(grad, x, p, alp_0, alp_l, tau=0.1, chi=9):
     """
     Block 1 in algorithm for inexact line search (see p. 48 in lecture notes)
 
@@ -55,8 +61,8 @@ def ls_block1(self, x, p, alp_0, alp_l, tau=0.1, chi=9):
     :return: updated alp_0 and alp_l
     """
     # Extrapolate
-    d_alp_0 = (alp_0 - alp_l) * (self.grad_alpha(alp_0, x, p)) / \
-              (self.grad_alpha(alp_l, x, p) - self.grad_alpha(alp_0, x, p))
+    d_alp_0 = (alp_0 - alp_l) * (grad_alpha(grad, alp_0, x, p)) / \
+              (grad_alpha(grad, alp_l, x, p) - grad_alpha(grad, alp_0, x, p))
     d_alp_0 = max([d_alp_0, tau * (alp_0 - alp_l)])
     d_alp_0 = min([d_alp_0, chi * (alp_0 - alp_l)])
     alp_l = alp_0
@@ -65,7 +71,7 @@ def ls_block1(self, x, p, alp_0, alp_l, tau=0.1, chi=9):
     return alp_0, alp_l
 
 
-def ls_block2(self, x, p, alp_0, alp_l, alp_u, tau=0.1):
+def ls_block2(func, grad, x, p, alp_0, alp_l, alp_u, tau=0.1):
     """
      Block 2 in algorithm for inexact line search (see p. 48 in lecture notes)
 
@@ -79,9 +85,9 @@ def ls_block2(self, x, p, alp_0, alp_l, alp_u, tau=0.1):
     """
     alp_u = min([alp_0, alp_u])
     # Interpolate
-    alp_0_bar = ((alp_0 - alp_l) ** 2 * self.grad_alpha(alp_l, x, p)) / \
-                2 * (self.func_alpha(alp_l, x, p) - self.func_alpha(alp_0, x, p) \
-                     + (alp_0 - alp_l) * self.grad_alpha(alp_l, x, p))
+    alp_0_bar = ((alp_0 - alp_l) ** 2 * grad_alpha(grad, alp_l, x, p)) / \
+                2 * (func_alpha(func, alp_l, x, p) - func_alpha(func, alp_0, x, p) \
+                     + (alp_0 - alp_l) * grad_alpha(grad, alp_l, x, p))
     alp_0_bar = max([alp_0_bar, alp_l + tau * (alp_u - alp_l)])
     alp_0_bar = min([alp_0_bar, alp_u - tau * (alp_u - alp_l)])
     alp_0 = alp_0_bar
@@ -89,7 +95,7 @@ def ls_block2(self, x, p, alp_0, alp_l, alp_u, tau=0.1):
     return alp_0, alp_u
 
 
-def ls_gold(self, p, x, alp_0=1, rho=0.25):
+def ls_gold(func, grad, x, p, alp_0=1, rho=0.25):
     """
     Line search with Goldstein conditions
 
@@ -108,27 +114,27 @@ def ls_gold(self, p, x, alp_0=1, rho=0.25):
         print('Your choice of rho was out of bounds (allowed bounds [0, 0.5]), it has now been set to 0.1')
 
     # Check if lc (left condition) and rc (right condition) is true or false
-    lc = (self.func_alpha(alp_0, x, p) >= self.func_alpha(alp_l, x, p) + (1 - rho) * (alp_0 - alp_u) \
-          * self.grad_alpha(alp_l, x, p))  # self.grad*p = dfunc/dalp
-    rc = (self.func_alpha(alp_0, x, p) <= self.func_alpha(alp_l, x, p) + rho * (alp_0 - alp_u) * \
-          self.grad_alpha(alp_l, x, p))
+    lc = (func_alpha(func, alp_0, x, p) >= func_alpha(func, alp_l, x, p) + (1 - rho) * (alp_0 - alp_u) \
+          * grad_alpha(grad, alp_l, x, p))
+    rc = (func_alpha(func, alp_0, x, p) <= func_alpha(func, alp_l, x, p) + rho * (alp_0 - alp_u) * \
+          grad_alpha(grad, alp_l, x, p))
 
     # While lc or rc is false, update alp_l and alp_0 or alp_u and alp_0 respectively
     while ~(lc & rc):
         if ~lc:
-            alp_0, alp_l = self.ls_block1(x, p, alp_0, alp_l)
+            alp_0, alp_l = ls_block1(grad, x, p, alp_0, alp_l)
         # if ~rc:
         else:
-            alp_0, alp_u = self.ls_block2(x, p, alp_0, alp_l, alp_u)
-        lc = (self.func_alpha(alp_0, x, p) >= self.func_alpha(alp_l, x, p) + \
-              (1. - rho) * (alp_0 - alp_u) * self.grad_alpha(alp_l, x, p))
-        rc = (self.func_alpha(alp_0, x, p) <= self.func_alpha(alp_l, x, p) + \
-              rho * (alp_0 - alp_u) * self.grad_alpha(alp_l, x, p))
+            alp_0, alp_u = ls_block2(func, grad, x, p, alp_0, alp_l, alp_u)
+        lc = (func_alpha(func, alp_0, x, p) >= func_alpha(func, alp_l, x, p) + \
+              (1. - rho) * (alp_0 - alp_u) * grad_alpha(grad, alp_l, x, p))
+        rc = (func_alpha(func, alp_0, x, p) <= func_alpha(func, alp_l, x, p) + \
+              rho * (alp_0 - alp_u) * grad_alpha(grad, alp_l, x, p))
 
     return alp_0
 
 
-def ls_wolfe(self, p, x, alp_0=1, rho=0.1, sigma=0.7):
+def ls_wolfe(func, grad, x, p, alp_0=1, rho=0.1, sigma=0.7):
     """
     Line search with Wolfe-Powell conditions
 
@@ -156,21 +162,20 @@ def ls_wolfe(self, p, x, alp_0=1, rho=0.1, sigma=0.7):
         print('Your choice of sigma was inaccurate since sigma > rho, it has now been set to 0.7')
 
     # Check if lc (left condition) and rc (right condition) is true or false
-    lc = (self.grad_alpha(alp_0, x, p) >= sigma * self.grad_alpha(alp_l, x, p))  # self.grad*p = dfunc/dalp
-    rc = (
-    self.func_alpha(alp_0, x, p) <= self.func_alpha(alp_l, x, p) + rho * (alp_0 - alp_u) * self.grad_alpha(alp_l, x, p))
+    lc = (grad_alpha(grad, alp_0, x, p) >= sigma * grad_alpha(grad, alp_l, x, p))  # grad*p = dfunc/dalp
+    rc = (func_alpha(func, alp_0, x, p) <= func_alpha(func, alp_l, x, p) + rho * (alp_0 - alp_u) * grad_alpha(grad, alp_l, x, p))
 
     # While lc or rc is false, update alp_l and alp_0 or alp_u and alp_0 respectively
     while ~(lc & rc):
         if ~lc:
-            alp_0, alp_l = self.ls_block1(x, p, alp_0, alp_l)
+            alp_0, alp_l = ls_block1(grad, x, p, alp_0, alp_l)
         # if ~rc:
         else:
-            alp_0, alp_u = self.ls_block2(x, p, alp_0, alp_l, alp_u)
-        lc = (self.func_alpha(alp_0, x, p) >= self.func_alpha(alp_l, x, p) + \
-              (1. - rho) * (alp_0 - alp_u) * self.grad_alpha(alp_l, x, p))
-        rc = (self.func_alpha(alp_0, x, p) <= self.func_alpha(alp_l, x, p) + \
-              rho * (alp_0 - alp_u) * self.grad_alpha(alp_l, x, p))
+            alp_0, alp_u = ls_block2(func, grad, x, p, alp_0, alp_l, alp_u)
+        lc = (func_alpha(func, alp_0, x, p) >= func_alpha(func, alp_l, x, p) + \
+              (1. - rho) * (alp_0 - alp_u) * grad_alpha(grad, alp_l, x, p))
+        rc = (func_alpha(func, alp_0, x, p) <= func_alpha(func, alp_l, x, p) + \
+              rho * (alp_0 - alp_u) * grad_alpha(grad, alp_l, x, p))
 
     return alp_0
 
