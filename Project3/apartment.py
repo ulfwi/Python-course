f10879e3d9c19979b9de6ead981e72e9363fc331
fannyandersson <a.fanny.andersson@gmail.com>
2017-10-06 13:00:25 +0200

    pushilipush alltihop

--- Project3/apartment.py
+++ Project3/apartment.py
@@ -4,11 +4,12 @@ from mpi4py import MPI
 from room import Room, RoomOne, RoomTwo, RoomThree
 
 
-class DirichletNeumann:
+class Apartment:
 
-    def __init__(self, dx=0.05, temp_wall=15, temp_window=5, temp_heater=40, omega=0.8):
-        self.dx = dx
-        self.temp_wall = temp_wall
+    def __init__(self, r1, r2, r3, omega=0.8):
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
         self.omega=omega
 
         if MPI.COMM_WORLD.Get_size() != 2:
@@ -17,10 +18,9 @@ class DirichletNeumann:
     def dirichlet_neumann(self, u1, u2, u3, maxit=10):
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
-        size = comm.Get_size()
 
         if rank == 0:
-            N = 1/self.dx # number of square elements.              of row/col for room 1 and 3, as well as row for room 2 (col=2*n)
+            N = 1/self.dx  # number of square elements.
 
             # Create rooms
             r1 = RoomOne(u1)
@@ -41,7 +41,7 @@ class DirichletNeumann:
                 r1.get_b(r2.u(r2.gamma))              # update_b?
                 r1.u = la.solve(r1.a, r1.b)
 
-                #Recieve the data from process 1
+                # Receive the data from process 1
                 st = MPI.Status()
                 r3 = comm.recv(source=1, tag=0, status=st)
 
@@ -49,14 +49,13 @@ class DirichletNeumann:
                 r3.get_b(r2.u(r2.gamma))
                 r3.u = la.solve(r3.a, r3.b)
 
-                #Send the data to process 0
+                # Send the data to process 0
                 comm.send(r3, dest=0, tag=0)
 
             # Relaxation
             r1.u = self.omega * r1.u + (1 - self.omega) * u1_old
             r2.u = self.omega * r2.u + (1 - self.omega) * u2_old
             r3.u = self.omega * r3.u + (1 - self.omega) * u3_old
-
 
     def solve_domain(self):
         pass
--- Project3/main.py
+++ Project3/main.py
@@ -1,3 +1,14 @@
-import dirichlet_neumann as dn
+from apartment import Apartment
+from room import Room, RoomOne, RoomTwo, RoomThree
 
-new = dn.DirichletNeumann
+
+
+# To create rooms
+r1 = RoomOne(dx=0.05, temp_wall=15, temp_window=5, temp_heater=40, temp_inside=20)
+r2 = RoomOne(dx=0.05, temp_wall=15, temp_window=5, temp_heater=40, temp_inside=20)
+r3 = RoomOne(dx=0.05, temp_wall=15, temp_window=5, temp_heater=40, temp_inside=20)
+
+flat = Apartment(r1, r2, r3)
+
+
+dn.dx
